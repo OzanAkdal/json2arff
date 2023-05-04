3d44c3006a0257180397c44ae630b41bf55f5225
Ozan Akdal <akdalozan@gmail.com>
2023-05-05 02:57:12 +1000

    some improvements

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
+# Facial Expression Recognition
 
+This project converts feature and label data into an ARFF format file for facial expression recognition.
+
+## Installation
+
+1. Clone the repository:
+   `git clone https://github.com/example/facial-expression-recognition.git`
+2. Install the required packages:
+   `pip install -r requirements.txt`
+
+## Usage
+
+`python facial_expression_recognition.py [FEATURE_FILE] [LABEL_FILE] [OUTPUT_FILE] [--config CONFIG_FILE] [--scale {minmax,standard}]`
+
+### Arguments
+
+- **FEATURE_FILE**: Path to the file containing feature data.
+- **LABEL_FILE**: Path to the file containing label data.
+- **OUTPUT_FILE**: Path to the output ARFF file.
+- **--config**: Path to the configuration file (default: config.yaml).
+- **--scale**: Apply scaling to numeric features. Choices: minmax, standard.
+
+### Configuration
+
+The **config.yaml** file contains the following configuration settings:
+
+- **relation_name**: Name of the relation in the ARFF file.
+- **class_name**: Name of the class attribute in the ARFF file.
+- **class_values**: Possible values for the class attribute.
+- **feature_key_format**: Format string for the feature data keys in the input files.
+- **label_key_format**: Format string for the label data keys in the input files.
+- **feature_data_types**: Data types of the feature attributes in the ARFF file.
--- json2arff.py
+++ json2arff.py
@@ -1,17 +1,127 @@
 import json
+import argparse
+import os
+import yaml
+from typing import Dict
 
-with open('feature_name_data.json', 'r') as sd:
-    dat = json.load(sd)
-with open('feature_name_labeltxt.json', 'r') as sl:
-    lab = json.load(sl)
-dosya = open("feature_name_feature.arff","w")
-dosya.write("@RELATION facialexpressionrecognition\n")
-oznitelik = int(dat["image_feature_0"]["cols"])
-for i in range(oznitelik):
-    dosya.write("@ATTRIBUTE feature"+str(i)+" real\n")
-dosya.write("@ATTRIBUTE duygu {Angry,Disgusted,Fear,Happy,Sad,Surprised}\n@data\n")
-for i in range(len(dat)):
-    dosya.write(",".join(map(str, dat["image_feature_"+str(i)]["data"])))
-    dosya.write("," + str(lab["image_label_" + str(i)]))
-    dosya.write("\n")
-dosya.close()
+
+class DataReader:
+    """Reads data from feature and label files."""
+
+    @staticmethod
+    def read_data(file_path: str):
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+
+
+class DataValidator:
+    """Validates feature and label data."""
+
+    @staticmethod
+    def validate_data(feature_data, label_data):
+        if len(feature_data) != len(label_data):
+            raise ValueError("Feature and label data lengths do not match.")
+
+
+class ARFFWriter:
+    """Writes feature and label data to an ARFF file."""
+
+    def __init__(self, feature_data, label_data, output_file, config):
+        self.feature_data = feature_data
+        self.label_data = label_data
+        self.output_file = output_file
+        self.config = config
+
+    def write_arff_file(self):
+        with open(self.output_file, "w") as file:
+            self.write_relation(file)
+            self.write_attributes(file)
+            self.write_data(file)
+
+    def write_relation(self, file):
+        file.write(f"@RELATION {self.config['relation_name']}\n\n")
+
+    def write_attributes(self, file):
+        for i, feature_dtype in enumerate(self.config['feature_data_types']):
+            file.write(f"@ATTRIBUTE feature{i} {feature_dtype}\n")
+        file.write(
+            f"@ATTRIBUTE {self.config['class_name']} {{{','.join(self.config['class_values'])}}}\n\n")
+
+    def write_data(self, file):
+        file.write("@DATA\n")
+        for i in range(len(self.feature_data)):
+            data_key = self.config['feature_key_format'].format(i)
+            file.write(",".join(map(str, self.feature_data[data_key]["data"])))
+            file.write(
+                "," + str(self.label_data[self.config['label_key_format'].format(i)]))
+            file.write("\n")
+
+
+class FeatureScaler:
+    """Scales numeric features using Min-Max scaling or standardization."""
+
+    def __init__(self, method, config):
+        self.method = method
+        self.config = config
+
+    def scale_features(self, feature_data):
+        for i in range(len(feature_data)):
+            data_key = self.config['feature_key_format'].format(i)
+            values = feature_data[data_key]['data']
+            if self.method == 'minmax':
+                min_value, max_value = min(values), max(values)
+                values = [(x - min_value) / (max_value - min_value)
+                          for x in values]
+            elif self.method == 'standard':
+                mean_value = sum(values) / len(values)
+                std_dev = (
+                    sum((x - mean_value) ** 2 for x in values) / len(values)) ** 0.5
+                values = [(x - mean_value) / std_dev for x in values]
+            else:
+                return feature_data
+            feature_data[data_key]['data'] = values
+        return feature_data
+
+
+def read_config_file(file_path: str) -> Dict:
+    with open(file_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+
+def get_args() -> argparse.Namespace:
+    """
+    Parses command line arguments.
+
+    Returns:
+        argparse.Namespace: The parsed command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="Convert feature and label data to ARFF format.")
+    parser.add_argument(
+        "feature_file",
+        nargs="?",
+        help="Path to the file containing feature data."
+    )
+    parser.add_argument(
+        "label_file",
+        nargs="?",
+        help="Path to the file containing label data."
+    )
+    parser.add_argument(
+        "output_file",
+        nargs="?",
+        help="Path to the output ARFF file."
+    )
+    parser.add_argument(
+        "--config",
+        default="config.yaml",
+        help="Path to the configuration file (default: 'config.yaml')."
+    )
+    parser.add_argument(
+        "--scale",
+        choices=["minmax", "standard"],
+        help="Apply scaling to numeric features. Choices: 'minmax', 'standard'."
+    )
+    return parser.parse_args()
